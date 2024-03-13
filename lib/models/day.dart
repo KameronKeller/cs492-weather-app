6e7b5d5d4236c8e4013251e9bd265b5457418729
Kameron Keller <mrkameronkeller@gmail.com>
2024-03-12 22:02:01 -0700

    Create day class to hold forecast data

---
+++ lib/models/day.dart
@@ -0,0 +1,41 @@
+import 'package:fl_chart/fl_chart.dart';
+
+class Day {
+
+  Map daysOfWeek = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday",
+  };
+
+  int high;
+  int low;
+  final int indexOfWeek;
+  final String shortForecast;
+  final String iconUrl;
+  final List<FlSpot> hourlyTempForecast;
+  // final bool isToday;
+  // wx icon
+  // final ? weatherIcon;
+
+  Day({
+    // set defaults at
+    required this.high,
+    required this.low,
+    required this.indexOfWeek,
+    required this.shortForecast,
+    required this.iconUrl,
+    required this.hourlyTempForecast
+    // required this.isToday,
+    // required this.weatherIcon,
+  });
+
+  String get dayOfWeek {
+    return daysOfWeek[indexOfWeek];
+  }
+
+}
