176ad0d3abecf59732dde0f7e6eaddaed4e1eba5
Kameron Keller <mrkameronkeller@gmail.com>
2024-03-12 22:04:18 -0700

    Move forecast widget to own file

---
+++ lib/widgets/forecast_widget.dart
@@ -0,0 +1,30 @@
+import 'package:cs492_weather_app/components/weatherScreen/weather_screen.dart';
+import 'package:cs492_weather_app/models/user_location.dart';
+import 'package:cs492_weather_app/models/weather_forecast.dart';
+import 'package:cs492_weather_app/widgets/description_widget.dart';
+import 'package:cs492_weather_app/widgets/location_text_widget.dart';
+import 'package:cs492_weather_app/widgets/temperature_widget.dart';
+import 'package:flutter/material.dart';
+
+class ForecastWidget extends StatelessWidget {
+  final UserLocation location;
+  final List<WeatherForecast> forecasts;
+  final BuildContext context;
+
+  const ForecastWidget(
+      {super.key,
+      required this.context,
+      required this.location,
+      required this.forecasts});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        LocationTextWidget(location: location),
+        TemperatureWidget(forecasts: forecasts),
+        DescriptionWidget(forecasts: forecasts)
+      ],
+    );
+  }
+}
