84fc8ab9798536f9777ac1a9cb1345fa846948f9
Kameron Keller <mrkameronkeller@gmail.com>
2024-03-12 22:02:43 -0700

    Move forecast widget to its own file

--- lib/components/weatherScreen/weather_screen.dart
+++ lib/components/weatherScreen/weather_screen.dart
@@ -1,4 +1,6 @@
 import 'package:cs492_weather_app/models/weather_forecast.dart';
+import 'package:cs492_weather_app/widgets/forecast_widget.dart';
+import 'package:cs492_weather_app/widgets/location_widget.dart';
 import '../../models/user_location.dart';
 import 'package:flutter/material.dart';
 import '../location/location.dart';
@@ -29,117 +31,5 @@ class _WeatherScreenState extends State<WeatherScreen> {
             location: widget.getLocation(),
             forecasts: widget.getForecastsHourly())
         : LocationWidget(widget: widget));
-  }
-}
-
-class ForecastWidget extends StatelessWidget {
-  final UserLocation location;
-  final List<WeatherForecast> forecasts;
-  final BuildContext context;
-
-  const ForecastWidget(
-      {super.key,
-      required this.context,
-      required this.location,
-      required this.forecasts});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        LocationTextWidget(location: location),
-        TemperatureWidget(forecasts: forecasts),
-        DescriptionWidget(forecasts: forecasts)
-      ],
-    );
-  }
-}
-
-class DescriptionWidget extends StatelessWidget {
-  const DescriptionWidget({
-    super.key,
-    required this.forecasts,
-  });
-
-  final List<WeatherForecast> forecasts;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 25,
-      width: 500,
-      child: Center(
-          child: Text(forecasts.elementAt(0).shortForecast,
-              style: Theme.of(context).textTheme.bodyMedium)),
-    );
-  }
-}
-
-class TemperatureWidget extends StatelessWidget {
-  const TemperatureWidget({
-    super.key,
-    required this.forecasts,
-  });
-
-  final List<WeatherForecast> forecasts;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 500,
-      height: 60,
-      child: Center(
-        child: Text('${forecasts.elementAt(0).temperature}º',
-            style: Theme.of(context).textTheme.displayLarge),
-      ),
-    );
-  }
-}
-
-class LocationTextWidget extends StatelessWidget {
-  const LocationTextWidget({
-    super.key,
-    required this.location,
-  });
-
-  final UserLocation location;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: SizedBox(
-        width: 500,
-        child: Text("${location.city}, ${location.state}, ${location.zip}",
-            style: Theme.of(context).textTheme.headlineSmall),
-      ),
-    );
-  }
-}
-
-class LocationWidget extends StatelessWidget {
-  const LocationWidget({
-    super.key,
-    required this.widget,
-  });
-
-  final WeatherScreen widget;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 400,
-      child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text("Requires a location to begin"),
-          ),
-          Location(
-              setLocation: widget.setLocation,
-              getLocation: widget.getLocation),
-        ],
-      ),
-    );
   }
 }
